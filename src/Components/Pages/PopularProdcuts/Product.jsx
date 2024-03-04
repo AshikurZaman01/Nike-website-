e1f1e74b6ca57d072343704087a70cfb003dd155
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-03-04 22:37:46 +0600

    add card data

--- src/Components/Pages/PopularProdcuts/Product.jsx
+++ src/Components/Pages/PopularProdcuts/Product.jsx
@@ -10,6 +10,10 @@ const Product = ({ product }) => {
             <div>
                 <img src={imgURL} alt="" />
             </div>
+            <div className="flex justify-between px-2 pt-2 text-xl ">
+                <h3 className="text-pink-500">{name}</h3>
+                <p className="text-blue-700 font-bold">{price}</p>
+            </div>
         </div>
     );
 };
